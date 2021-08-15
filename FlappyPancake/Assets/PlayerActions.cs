e49a1feba7474e31f78ceec5d18515bf99df0f30
Dero <petar.mami@gmail.com>
2021-08-15 13:19:19 +0200

    Player Input and Action
    I've set up the player input event system and actions for the player which subscribe to input events

---
+++ FlappyPancake/Assets/PlayerActions.cs
@@ -0,0 +1,47 @@
+﻿/*====================================================================*/
+/*  This script holds player actions to be executed on input command  */
+/*====================================================================*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    public float JumpForce;
+
+    private Rigidbody2D _rb;
+    private bool _jump = false;
+
+    /*  Singleton start */
+    public static PlayerActions instance;
+    private void Awake() { instance = this; }
+    /*  Singleton end   */
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        PlayerInputSystem.instance.JumpInput += Jump;
+    }
+
+    void FixedUpdate()
+    {
+        Jumpy(_jump);
+        _jump = false;
+    }
+
+    void Jump()
+    {
+        _jump = true;
+    }
+
+    void Jumpy(bool jump)
+    {
+        if (jump)
+        {
+            _rb.velocity = Vector2.zero; // to keep a constant jump height
+            _rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+}
---
+++ FlappyPancake/Assets/PlayerInputSystem.cs
@@ -0,0 +1,33 @@
+﻿/*==============================================================*/
+/*  This script holds input events to be used by other scripts  */
+/*==============================================================*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputSystem : MonoBehaviour
+{
+    public event Action JumpInput;
+    
+    /*  Singleton start */
+    public static PlayerInputSystem instance;
+    private void Awake() { instance = this; }
+    /*  Singleton end   */
+
+    void Update()
+    {
+        Jump();
+    }
+
+
+    void Jump()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            JumpInput?.Invoke();
+        }
+    }
+
+}
+
